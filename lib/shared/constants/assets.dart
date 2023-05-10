98f1a150c1dfc8c067c31930782076cd8c3d1f90
Muhammad Rizky Nur Fajrie <86530834+mrizkynurfajrie@users.noreply.github.com>
2023-05-10 17:24:17 +0800

    add assets

---
+++ lib/shared/constants/assets.dart
@@ -0,0 +1,5 @@
+class AppIcons {
+  static const userList = 'assets/icons/user-list.svg';
+  static const userAdd = 'assets/icons/user-add.svg';
+  static const userAbout = 'assets/icons/user-about.svg';
+}
